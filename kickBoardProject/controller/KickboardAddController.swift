4138e5594a7233d2815453b9501d9b5d48018cfe
sh990920 <sh990920@gmail.com>
2024-07-23 16:11:33 +0900

    Create KickboardAddViewController

---
+++ kickBoardProject/controller/KickboardAddController.swift
@@ -0,0 +1,19 @@
+//
+//  AddKickboardController.swift
+//  kickBoardProject
+//
+//  Created by 박승환 on 7/23/24.
+//
+
+import UIKit
+
+class KickboardAddController: UIViewController {
+    
+    let kickBoardAddView = KickBoardAddView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = kickBoardAddView
+    }
+    
+}
