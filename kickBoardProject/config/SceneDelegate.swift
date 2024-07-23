ca24e503d8695c47ae850b5fc9b87db5145a3300
soobeen27 <youdew27@gmail.com>
2024-07-23 15:24:07 +0900

    remove test file

--- kickBoardProject/config/SceneDelegate.swift
+++ kickBoardProject/config/SceneDelegate.swift
@@ -9,8 +9,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
  
-        let loginVC = LoginViewController()
-        window.rootViewController = UINavigationController(rootViewController: loginVC)
+//        let loginVC = LoginViewController()
+//        window.rootViewController = UINavigationController(rootViewController: loginVC)
         window.makeKeyAndVisible()
         self.window = window
     }
--- kickBoardProject/controller/TabBarController.swift
+++ kickBoardProject/controller/TabBarController.swift
@@ -15,15 +15,15 @@ class TabBarController: UITabBarController {
         self.tabBar.tintColor = .black
         
         let myPageVC = UINavigationController(rootViewController: MyPageViewController())
-        let vc = UINavigationController(rootViewController: ViewController())
-        let kickBoardVC = UINavigationController(rootViewController: KickBoardRegisterViewController())
+//        let vc = UINavigationController(rootViewController: ViewController())
+//        let kickBoardVC = UINavigationController(rootViewController: KickBoardRegisterViewController())
 
         
-        kickBoardVC.tabBarItem = UITabBarItem(title: "등록", image: UIImage(systemName: "plus.square"), selectedImage: UIImage(systemName: "plus.square.fill"))
-        vc.tabBarItem = UITabBarItem(title: "지도", image: UIImage(systemName: "map"), selectedImage: UIImage(systemName: "map.fill"))
+//        kickBoardVC.tabBarItem = UITabBarItem(title: "등록", image: UIImage(systemName: "plus.square"), selectedImage: UIImage(systemName: "plus.square.fill"))
+//        vc.tabBarItem = UITabBarItem(title: "지도", image: UIImage(systemName: "map"), selectedImage: UIImage(systemName: "map.fill"))
         myPageVC.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person.crop.circle"), selectedImage: UIImage(systemName: "person.crop.circle.fill"))
         
         //탭바에 등록할 컨트롤러 추가
-        self.setViewControllers([vc, kickBoardVC, myPageVC], animated: true)
+//        self.setViewControllers([vc, kickBoardVC, myPageVC], animated: true)
     }
 }
