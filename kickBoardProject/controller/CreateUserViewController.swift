7de1cca3e6e87e295751d7b68ea2d1b27899e678
LikelyLime <qorwlgml2003@naver.com>
2024-07-24 16:03:46 +0900

    [20240724] 회원가입 구현

--- kickBoardProject/controller/CreateUserViewController.swift
+++ kickBoardProject/controller/CreateUserViewController.swift
@@ -7,14 +7,42 @@
 
 import Foundation
 import UIKit
+import FirebaseFirestore
 class CreateUserViewController: UIViewController{
     let createUserView = CreateUserView()
+    var db: Firestore!
+    var userModel: UserModel?
     override func loadView() {
         view = createUserView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        db = Firestore.firestore()
+        setAction()
     }
+    
+    func setAction(){
+        createUserView.createButton.addTarget(self, action: #selector(createUserTapped), for: .touchDown)
+    }
+    
+    @objc func createUserTapped(){
+        
+        db.collection("users").document(createUserView.emailTextField.text!).setData([
+            "autoLoginYn": "N",
+            "email": "\(createUserView.emailTextField.text!)",
+            "lentalYn": "N",
+            "name": "\(createUserView.nameTextField.text!)",
+            "pwd": "\(createUserView.pwdTextField.text!)"
+        ]){ err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
+    
 }
