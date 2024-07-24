5647cfe4bce97c77e2fc16f6b2a4b6a6401833ad
sh990920 <sh990920@gmail.com>
2024-07-24 16:08:21 +0900

    ToolBar에 KickBoardAddView 추가

---
+++ kickBoardProject/view/Kickboard.swift
@@ -0,0 +1,17 @@
+//
+//  Kickboard.swift
+//  kickBoardProject
+//
+//  Created by 박승환 on 7/24/24.
+//
+
+import Foundation
+
+struct Kickboard {
+    let id: String
+    let userEmail: String
+    let battery: String
+    let status: String
+    let latitude: String
+    let longitude: String
+}
