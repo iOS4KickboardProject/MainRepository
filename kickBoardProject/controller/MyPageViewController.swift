954fe9db5dfe604f41a11f86cf9ba3a4213d58e3
soobeen27 <youdew27@gmail.com>
2024-07-24 16:42:21 +0900

    my page layout updated

--- kickBoardProject/controller/MyPageViewController.swift
+++ kickBoardProject/controller/MyPageViewController.swift
@@ -10,31 +10,65 @@ import SnapKit
 
 class MyPageViewController: UIViewController {
     
-    lazy var logoutBtn: UIBarButtonItem = {
-        let btn = UIBarButtonItem()
-        btn.title = "logout"
-        btn.style = .plain
-        btn.target = self
-        btn.action = #selector(logoutTapped)
-        return btn
-    }()
+    var myPageView: MyPageView!
+    
+    let kickBoardItems = ["first kickboard", "second kickboard", "third kickboard", "fourth kickboard", "fiveth kickboard"]
+    let useItems = ["first used 1000$", "second usage 500$", "third usage 700$", "fourth usage 800$", "fiveth usage 900$"]
+    
+    override func loadView() {
+        super.loadView()
+        myPageView = MyPageView(frame: self.view.frame)
+        self.view = myPageView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setNav()
+        setTableView()
+    }
+    
+    func setTableView() {
+        myPageView.kickboardTableView.dataSource = self
+        myPageView.kickboardTableView.delegate = self
+        myPageView.useTableView.dataSource = self
+        myPageView.useTableView.delegate = self
     }
     
     func setNav() {
         self.navigationItem.title = "my page"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.tintColor = .black
         self.navigationController?.navigationBar.largeTitleTextAttributes = [ .foregroundColor : UIColor.black]
-        navigationItem.rightBarButtonItem = logoutBtn
+        let logoutButton = UIBarButtonItem(title: "로그아웃", style: .plain, target: self, action: #selector(logoutTapped))
+        navigationItem.rightBarButtonItem = logoutButton
     }
     
     @objc
     func logoutTapped() {
         guard let backVC = self.tabBarController?.navigationController else { return }
         backVC.popToRootViewController(animated: true)
+    }
+}
+
+extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == myPageView.kickboardTableView {
+            return kickBoardItems.count
+        } else if tableView == myPageView.useTableView {
+            return useItems.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        if tableView == myPageView.kickboardTableView {
+            cell.textLabel?.text = kickBoardItems[indexPath.row]
+        } else if tableView == myPageView.useTableView {
+            cell.textLabel?.text = useItems[indexPath.row]
+        }
+        return cell
     }
 }
