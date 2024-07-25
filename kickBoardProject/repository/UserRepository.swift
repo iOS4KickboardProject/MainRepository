8af1ed354a80b4f872b6bfc4a5ca60b615702f92
dx._.xk7 <polcet112@gmail.com>
2024-07-25 19:02:45 +0900

    서버패치

--- kickBoardProject/repository/UserRepository.swift
+++ kickBoardProject/repository/UserRepository.swift
@@ -7,13 +7,20 @@
 
 import Foundation
 import FirebaseFirestore
-class UserRepository{
+import KakaoMapsSDK
+
+class UserRepository {
     var db: Firestore!
     var user: UserStruct!
     
+    //    var poiPositions: [MapPoint] = []
+    
+    var poiPositions: [MapPoint] = []
+    
+    
     init() {
-       db = Firestore.firestore()
-   }
+        db = Firestore.firestore()
+    }
     //데이터 조회
     func retrieveUserData(email: String, completion: @escaping (UserStruct?) -> Void){
         db.collection("users").document(email).getDocument { [weak self] (document, error) in
@@ -38,4 +45,65 @@ class UserRepository{
             }
         }
     }
+    
+    func createPoi_Data(poi_ID: String, long: Double, lati: Double) {
+        // Prepare the data to be uploaded
+        let data: [String: Any] = [
+            "map": [
+                "longitude": "\(long)",
+                "latitude": "\(lati)"
+            ]
+        ]
+        // Set the data with merge option
+        db.collection("Poi").document(poi_ID).setData(data, merge: true) { [weak self] error in
+            if let error = error {
+                print("Error writing document \(poi_ID): \(error)")
+            } else {
+                print("Document \(poi_ID) successfully written!")
+                
+                // Add the new MapPoint to poiPositions
+                self?.poiPositions.append(MapPoint(longitude: long, latitude: lati))
+            }
+        }
+    }
+    
+    //    func createPoi_Data(poi_ID: String, long: Double, lati: Double) {
+    //           db.collection("Poi").document(poi_ID).setData(["map": ["longitude":"\(long)", "latitude": "\(lati)"]]) { error in
+    //               if let error = error {
+    //                   print("called Firebase:\(error)")
+    //               } else {
+    //                   print("success")
+    //                   self.poiPositions.append(MapPoint(longitude: long, latitude: lati))
+    //
+    //
+    //               }
+    //           }
+    //       }
+    func fetchAllPois() {
+        db.collection("Poi").getDocuments { [weak self] (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+                return
+            }
+            
+            for document in querySnapshot!.documents {
+                let data = document.data()
+                if let map = data["map"] as? [String: String],
+                   let longitudeStr = map["longitude"],
+                   let latitudeStr = map["latitude"],
+                   let longitude = Double(longitudeStr),
+                   let latitude = Double(latitudeStr) {
+                    print("Fatch Done")
+                    let mapPoint = MapPoint(
+                        longitude: longitude,
+                        latitude: latitude
+                    )
+                    self?.poiPositions.append(MapPoint(longitude: longitude, latitude: latitude))
+                }
+            }
+        }
+    }
 }
+
+
+
